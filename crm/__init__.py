9c2fc7aa54d77cc6297ffdaaf7779beacfe2476a
Son <sonnt103@fpt.com>
2025-04-24 16:40:37 +0700

    update tên

--- crm/__init__.py
+++ crm/__init__.py
@@ -1,4 +1,4 @@
-__title__ = "Frappe Portal"
+__title__ = "FPT Portal"
 __version__ = "1.0.0"
 
 import frappe
@@ -8,7 +8,6 @@ no_cache = 1
 def get_context(context):
     context.show_sidebar = True
     context.no_cache = 1
-    context.title = "Portal"
+    context.title = "FPT Portal"
     context.breadcrumbs = [{"label": "Portal", "route": "/portal"}]
     return context
-
